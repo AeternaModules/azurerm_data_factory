3bbef1169ebea2ed3813f232d571604b2f6e9ccb
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:01:21 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "data_factories_id" {
+  description = "Map of id values across all data_factories, keyed the same as var.data_factories"
+  value       = { for k, v in azurerm_data_factory.data_factories : k => v.id }
+}
 output "data_factories_customer_managed_key_id" {
   description = "Map of customer_managed_key_id values across all data_factories, keyed the same as var.data_factories"
   value       = { for k, v in azurerm_data_factory.data_factories : k => v.customer_managed_key_id }
